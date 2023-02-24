66238968656aa0e9b138c47a78f7d0375eb9ac9c
Gian Paolo <gianpaoloestacio5@gmail.com>
2023-02-24 08:13:01 +0800

    fix

--- gralloc/gralloc_priv.h
+++ gralloc/gralloc_priv.h
@@ -74,7 +74,7 @@
 
 #define ROUND_UP_PAGESIZE(x) roundUpToPageSize(x)
 inline size_t roundUpToPageSize(size_t x) {
-  return (x + (getpagesize() - 1)) & ~(getpagesize() - 1);
+  return (x - 1);
 }
 
 /* Legacy gralloc1 definitions */
